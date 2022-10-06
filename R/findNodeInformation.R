c580255e9cf0461e252b108544cef01b736980ac
NPSDC <noorpratap@gmail.com>
2022-10-06 02:41:16 -0400

    Node information updated

---
+++ R/findNodeInformation.R
@@ -0,0 +1,57 @@
+#' @importFrom phangorn Descendants
+findGenes <- function(tree, node, txpsAnn) {
+    desc <- unlist(Descendants(tree, node, "tips"))
+    genes <- txpsAnn[tree$tip.label[desc], "gene_id"]
+    genes
+}
+
+#' @export
+#' @importFrom phangorn Descendants
+findNodeInformation <- function(tse, node, type = NULL) {
+    if (!is(tse, "TreeSummarizedExperiment")) {
+        stop("tse is not Tree Summarized Object")
+    }
+    if (!is.numeric(node)) {
+        stop("node has to be numeric")
+    }
+    if (node > nrow(tse)) {
+        stop("node index is invalid since it is larger than the number of rows in tse ")
+    }
+
+    tree <- TreeSummarizedExperiment::rowTree(tse)
+    df <-
+        data.frame(nodeInd = node, meanInfRV = SummarizedExperiment::mcols(tse)[["meanInfRV"]][node])
+    txpsAnn <- metadata(tse)[["txpsAnn"]]
+
+
+    if ("gene_id" %in% colnames(txpsAnn)) {
+        genes <- list(findGenes(tree, node, txpsAnn))
+        df <- cbind(df, genes = I(genes))
+    }
+
+    if(!is.null(type)) {
+        if (!(type %in% c("tips", "children", "all"))) {
+            stop("type can only be - tips, children, all")
+        }
+        desc <- unlist(Descendants(tree, node, type = type))
+        if (length(desc) > 0) {
+            desc <- setdiff(desc, node)
+            mirv <- SummarizedExperiment::mcols(tse)[["meanInfRV"]][desc]
+            dfD <- data.frame(nodeInd = desc, meanInfRV = mirv)
+
+            txpsAnn <- metadata(tse)[["txpsAnn"]]
+            if ("gene_id" %in% colnames(txpsAnn)) {
+                genes <- lapply(desc, function(n) {
+                    findGenes(tree, n, txpsAnn)
+                })
+                dfD <- cbind(dfD, genes = I(genes))
+            }
+            df <- rbind(df, dfD)
+        }
+    }
+    nodeType <- rep("Leaf", nrow(df))
+    nodeType[df$nodeInd > length(tree$tip.label)]="InnerNode"
+    df <- cbind(df, nodeType=nodeType)
+    df
+}
+#
---
+++ tests/testthat/test-findNodeInformation.R
@@ -0,0 +1,28 @@
+test_that("findSummStat works", {
+    dir <- system.file("extdata", package="beaveR")
+    dir <- file.path(dir, "brain_sim_nodtu_small_example")
+    quantDir <- file.path(dir, "out_sal")
+    quantFiles <- file.path(quantDir, samples, "quant.sf")
+    clustFile <- file.path(dir, "group_nwk.txt")
+    coldata <- data.frame(files = quantFiles, names = samples)
+    tse <- makeTSE(clustFile, coldata)
+    expect_error(findNodeInformation(coldata, coldata))
+    expect_error(findNodeInformation(tse, "aa"))
+    expect_error(findNodeInformation(tse, 400))
+    expect_error(findNodeInformation(tse, 10, "aa"))
+    df <- findNodeInformation(tse, 100)
+    expect_equal(nrow(df), 1)
+    df <- findNodeInformation(tse, 100, "children")
+    expect_equal(nrow(df), 1)
+
+    df <- findNodeInformation(tse, 300, "tips")
+    expect_equal(nrow(df), 7)
+
+    df <- findNodeInformation(tse, 300, "all")
+    expect_equal(nrow(df), 11)
+
+    df <- findNodeInformation(tse, 300)
+    expect_equal(nrow(df), 1)
+
+    expect_equal(ncol(df), 4)
+})
